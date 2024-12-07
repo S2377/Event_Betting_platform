c503533c5ac014b958d3e82c869f3ffd8226c670
naren9521 <ns952140@gmail.com>
2024-12-07 09:51:03 +0530

    fixed merge conflict

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { fetchEvents } from '../api';  // Import the fetchEvents function
-<<<<<<< HEAD
+
 import './Home.css'; // Make sure to import the CSS file
-=======
-import './home.css'; // Make sure to import the CSS file
 import EventCard from '../components/EventCard'
->>>>>>> b15b95b (modified events page)
 
 const Home = () => {
     const [events, setEvents] = useState([]);
